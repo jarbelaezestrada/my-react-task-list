8276e19ebdf2978482322ec8e4b53788ea58bc7f
jarbelaezestrada <julianaarbelaezestrada@gmail.com>
2023-12-18 22:07:57 -0500

    Agregando funciones a mi proyecto

---
+++ my-react-task-list/review-1.md
@@ -0,0 +1,58 @@
+
+
+#####Descripción del Producto:
+
+#¡Bienvenido a "My React Task List"!
+
+Nuestra aplicación de lista de tareas con React es la herramienta perfecta para ayudarte a organizar tu vida diaria de manera eficiente. Diseñada para la simplicidad y la usabilidad, esta aplicación hace que gestionar tus tareas sea más fácil que nunca.
+
+###Características Destacadas:
+
+#####Gestión Intuitiva de Tareas:
+
+Añade nuevas tareas con solo un clic.
+Elimina las tareas completadas para mantener tu lista limpia y organizada.
+Edición Rápida y Sencilla:
+
+Modifica el título y la descripción de tus tareas en cuestión de segundos.
+Personaliza tus tareas según tus necesidades cambiantes.
+Colores Dinámicos:
+
+El cambio automático de color te indica visualmente el estado de cada tarea.
+La paleta de colores vibrantes facilita la identificación de tareas pendientes y completadas.
+Persistencia de Datos:
+
+Nunca pierdas tus tareas. La aplicación almacena automáticamente tus listas en localStorage.
+Inicia sesión y recupera tus tareas donde las dejaste, incluso después de cerrar la aplicación.
+Interfaz Amigable:
+
+Diseño limpio y amigable que hace que la gestión de tareas sea una experiencia placentera.
+Navegación sencilla para usuarios de todos los niveles.
+Beneficios:
+
+#####Organización sin Esfuerzo:
+
+ Olvídate de las listas de tareas confusas. Organiza tu día de manera fácil y eficiente.
+
+#####Aumenta la Productividad: 
+
+Al centrarte en tareas específicas y prioridades, podrás completar más en menos tiempo.
+
+
+#####Flexibilidad Personalizada: 
+
+Adapta la aplicación a tus necesidades cambiantes gracias a la edición rápida de tareas.
+
+
+#####Experiencia Continua: 
+
+Mantén tus listas de tareas siempre a tu alcance, incluso en diferentes sesiones de uso.
+Descarga "My React Task List" ahora y descubre cómo hacer que la gestión de tareas sea un placer. ¡Haz de cada día un día productivo!
+
+####"Espero que esta descripción refleje adecuadamente las características y beneficios de tu aplicación de lista de tareas con React. Si tienes detalles específicos que te gustaría destacar o ajustes que desees realizar, estaré encantado de adaptar la descripción según tus necesidades."
+
+
+
+
+
+
--- my-react-task-list/src/components/TaskList.jsx
+++ my-react-task-list/src/components/TaskList.jsx
@@ -1,5 +1,12 @@
+import React, { useState, useEffect } from 'react';
 import { Task } from "./Task";
 export const TaskList = () => {
+     const [tasks, setTasks] = useState([]);
+     useEffect(() => {
+    // Cargar las tareas desde localStorage al inicio de la aplicación
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(storedTasks);
+  }, []);
 
     const tareas  = [
         {id: 1, name:"Lavar los platos", completed: true},
@@ -9,7 +16,46 @@ export const TaskList = () => {
         {id: 5, name:"Lavarse los dientes", completed: false},
         
     ]
-    return <div>
+
+  const addTask = (title, description) => {
+    // Agregar nueva tarea al listado
+    const newTask = {
+      id: Date.now(),
+      title,
+      description,
+      completed: false,
+    };
+
+    setTasks(prevTasks => [...prevTasks, newTask]);
+
+    // Actualizar localStorage
+    localStorage.setItem('tasks', JSON.stringify([...tasks, newTask]));
+  };
+
+  const deleteTask = taskId => {
+    // Eliminar tarea del listado
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(updatedTasks);
+
+    // Actualizar localStorage
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
+  const editTask = (taskId, newTitle, newDescription) => {
+    // Modificar título y descripción de la tarea
+    const updatedTasks = tasks.map(task =>
+      task.id === taskId ? { ...task, title: newTitle, description: newDescription } : task
+    );
+
+    setTasks(updatedTasks);
+
+    // Actualizar localStorage
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
+  return (
+    <div>
+        <div>
 
          <Task name="lavar los platos"/>
          <Task name="Pasear al perro"/>
@@ -17,4 +63,21 @@ export const TaskList = () => {
          <Task name="Alimentar al gato"/>
          <Task name="lavarse los dientes"/>
     </div>
+      <h2>Task List</h2>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            <span>{task.title}</span>
+            <button onClick={() => deleteTask(task.id)}>Eliminar</button>
+            <button onClick={() => editTask(task.id, prompt('Nuevo título'), prompt('Nueva descripción'))}>
+              Editar
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => addTask(prompt('Título'), prompt('Descripción'))}>Agregar Tarea</button>
+    </div>
+  );
 }
+
+ 
